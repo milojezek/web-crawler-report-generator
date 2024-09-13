476f9240bd32bf111aac5a353bdd8aebb6b74741
Miloslav Jezek <milo.jezek@gmail.com>
2024-09-13 20:48:28 +0200

    Prepare CLI arguments proccessing

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello world");
+import { argv } from "node:process";
+
+/**
+ * Main function that process the CLI arguments
+ */
+const main = () => {
+	const cliCommands = argv.slice(2);
+
+	if (cliCommands.length < 1) {
+		console.error("No CLI argument");
+	} else if (cliCommands.length > 1) {
+		console.error("Too many CLI arguments");
+	} else {
+		console.log(cliCommands[0]);
+	}
+};
+
+main();
