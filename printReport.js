ded59007d6a8f8eb174e2751ecb09c9b2f10e2d5
Miloslav Jezek <milo.jezek@gmail.com>
2024-09-14 21:14:02 +0200

    Prepare function for printing report and helper function for report printing

---
+++ printReport.js
@@ -0,0 +1,34 @@
+/**
+ * Print the report of internal links
+ * @param pages the pages map to print
+ */
+const printReport = (pages) => {
+	console.log("Here's the report...");
+	const sortedPages = sortPages(pages);
+	for (const page of sortedPages) {
+		const url = page[0];
+		const count = page[1];
+		console.log(`Found ${count} internal links to ${url}`);
+	}
+};
+
+export { printReport };
+
+/**
+ * Sort the pages by count and then by URL
+ * @param pages the pages map to sort
+ * @returns the sorted pages array
+ */
+const sortPages = (pages) => {
+	// inner array: [url, count]
+	const pagesArr = Object.entries(pages);
+	pagesArr.sort((pageA, pageB) => {
+		if (pageB[1] === pageA[1]) {
+			return pageA[0].localeCompare(pageB[0]);
+		}
+
+		return pageB[1] - pageA[1];
+	});
+
+	return pagesArr;
+};
